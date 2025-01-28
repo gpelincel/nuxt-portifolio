11a9b87c07df7bf5583760221cdefcbd689d01dd
gpelincel <gabrielpelincel@gmail.com>
2025-01-28 11:07:59 -0300

    Feat: Home criada

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="flex justify-between p-4">
+        <div class="flex items-center justify-center font-semibold gap-2 hover:scale-104 transition-transform">
+            <i class="fi fi-rr-square-terminal text-3xl mb-[-5px]"></i>
+            <h1 class="uppercase text-xl">Gabriel Pelincel</h1>
+        </div>
+        <nav class="flex items-center gap-2">
+            <ul class="navbar flex gap-6 w-full uppercase font-semibold text-xl">
+                <li><a href="">Home</a></li>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Projects</a></li>
+            </ul>
+            <p class="text-5xl">|</p>
+            <ul class="flex gap-3 text-2xl mb-[-5px]">
+                <li><a href=""><i class="fi fi-brands-linkedin"></i></a></li>
+                <li><a href=""><i class="fi fi-brands-github"></i></a></li>
+                <li><a href=""><i class="fi fi-brands-whatsapp"></i></a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="grid grid-cols-2 items-center justify-items-center">
+        <img class="w-3/4" src="~/assets/img/web-development.gif" alt="">
+        <div>
+            <p>Fullstack Developer</p>
+            <h1 class="uppercase text-6xl font-semibold">Gabriel Pelincel</h1>
+            <!-- <h2 class="text-xl">Fullstack Developer</h2> -->
+            <hr class="my-3">
+            <div class="flex items-center gap-6 text-6xl">
+                <i class="devicon-php-plain"></i>
+                <i class="devicon-java-plain-wordmark"></i>
+                <i class="devicon-angular-plain"></i>
+                <i class="devicon-nuxtjs-plain"></i>
+                <i class="devicon-tailwindcss-original"></i>
+                <i class="devicon-mysql-plain-wordmark text-7xl"></i>
+            </div>
+        </div>
+    </section>
+</template>
