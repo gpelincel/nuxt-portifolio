6377623fbe3e6d99963f33986200baef7304851f
gpelincel <gabrielpelincel@gmail.com>
2025-02-17 15:01:04 -0300

    Feat: Área de experiência profissional

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -3,14 +3,14 @@
         <div class="rounded-full border border-2 border-white p-2">
             <img class="rounded-full" src="https://placehold.co/300x300" alt="">
         </div>
-        <div class="text-justify pr-12">
+        <div class="text-justify pr-12 text-lg">
             <h1 class="font-semibold uppercase text-5xl">Sobre mim</h1>
             <hr class="my-4">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam rerum explicabo dicta corporis a voluptas aliquam odit fuga ab ipsum impedit consequatur, vel itaque aspernatur molestias pariatur magni magnam quidem?</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi tenetur sapiente illum obcaecati fuga similique quaerat doloremque itaque eius maxime laboriosam vitae aliquid, iusto omnis! Cupiditate iusto beatae magni praesentium! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, tempore harum vitae sapiente esse delectus dolorem deleniti quod quaerat minima nesciunt molestiae tenetur. Iusto maiores vel blanditiis in voluptatem. Distinctio?</p>
+            <p>Desenvolvedor Fullstack com experiência em sistemas web e aplicativos mobile. Atuei na Global Data como estagiário e como júnior na RPV Tecnologia. Graduado em Desenvolvimento de Sistemas e cursando Engenharia de Computação, destaco-me pela adaptabilidade, resolução de problemas e trabalho em equipe.</p>
         </div>
     </div>
-    <div class="mt-8">
+    <div class="flex flex-col gap-16 mt-8">
         <Academics></Academics>
+        <Work></Work>
     </div>
 </template>
--- components/Work.vue
+++ components/Work.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="grid grid-cols-3 gap-4 justify-items-center items-center">
         <div class="flex flex-col items-center justify-center text-center">
-            <img class="invert w-1/3" src="~/assets/img/education.png" alt="">
-            <h1 class="uppercase text-2xl font-semibold">Formação Acadêmica</h1>
+            <img class="invert w-1/3" src="~/assets/img/coding.png" alt="">
+            <h1 class="uppercase text-2xl font-semibold">Experiência Profissional</h1>
         </div>
         <div class="space-y-2 border-r border-white">
-            <p class="opacity-75 text-xl">Técnino</p>
-            <h1 class="text-2xl font-bold">Desenvolvimento de Sistemas</h1>
+            <p class="opacity-75 text-xl">Global Data</p>
+            <h1 class="text-2xl font-bold">Estagiário Desenvolvimento Web</h1>
             <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed orci in justo mollis
                 vulputate. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
                 turpis egestas.</p>
         </div>
         <div class="space-y-2">
-            <p class="opacity-75 text-xl">Bacharelado</p>
-            <h1 class="text-2xl font-bold">Engenharia de Computação</h1>
+            <p class="opacity-75 text-xl">RPV Tecnologia</p>
+            <h1 class="text-2xl font-bold">Desenvovlvedor Fullstack</h1>
             <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed orci in justo mollis
                 vulputate. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
                 turpis egestas.</p>
